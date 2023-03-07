5fc0ca0e834e880b543fa807b8173e5a12181400
syobochim <syobochim@gmail.com>
2023-03-07 17:17:34 +0900

    Goのテストをtestifyを使って可読性を上げた

--- fizz-buzz-go/fizz-buzz_test.go
+++ fizz-buzz-go/fizz-buzz_test.go
@@ -1,28 +1,24 @@
 package fizzBuzz
 
-import "testing"
-import "reflect"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_fizzBuzz_3(t *testing.T) {
 	var result []string = fizzBuzz(3)
 	var expected []string = []string{"1", "2", "Fizz"}
-	if !reflect.DeepEqual(result, expected) {
-		t.Error("結果が違う, 期待する結果 ['1','2','Fizz'], 得られた結果 ", result)
-	}
+	assert.Equal(t, result, expected)
 }
 
 func Test_fizzBuzz_5(t *testing.T) {
 	var result []string = fizzBuzz(5)
 	var expected []string = []string{"1", "2", "Fizz", "4", "Buzz"}
-	if !reflect.DeepEqual(result, expected) {
-		t.Error("結果が違う, 期待する結果, ", expected, " 得られた結果 ", result)
-	}
+	assert.Equal(t, result, expected)
 }
 
 func Test_fizzBuzz_15(t *testing.T) {
 	var result []string = fizzBuzz(15)
 	var expected []string = []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}
-	if !reflect.DeepEqual(result, expected) {
-		t.Error("結果が違う, 期待する結果, ", expected, " 得られた結果 ", result)
-	}
+	assert.Equal(t, result, expected)
 }
