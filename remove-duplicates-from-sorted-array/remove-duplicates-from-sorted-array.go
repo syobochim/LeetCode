0cd2bae68c72b7a1a4b766579cb2bb980f9da749
syobochim <syobochim@gmail.com>
2023-03-12 22:36:09 +0900

    j を消して、連続した数を調べればOKという形にした

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -1,18 +1,12 @@
 package removeDuplicates
 
-import "fmt"
-
 func removeDuplicates(nums []int) int {
-	var resultIndex int = 1
-	for i := 0; i < len(nums); i++ {
-		for j := i; j < len(nums); j++ {
-			fmt.Printf("i: %d, j: %d, resultIndex: %d \n", i, j, resultIndex)
-			if nums[i] != nums[j] {
-				nums[resultIndex] = nums[j]
-				resultIndex++
-				i = j
-			}
-		}
-	}
-	return resultIndex
+    var resultIndex int = 1
+    for i := 0; i < len(nums) - 1; i++ {
+        if nums[i] != nums[i + 1] {
+            nums[resultIndex] = nums[i + 1]
+            resultIndex++
+        }
+    }
+    return resultIndex
 }
